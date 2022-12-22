d77c10228b15000b8f69a8793b75b22f0b99ee2f
ClementPremartin <clementpremartin@gmail.com>
2022-12-22 19:30:26 +0100

    style signIn page, add global css variables, and import font weight

--- web-app/src/styles/style-constants.ts
+++ web-app/src/styles/style-constants.ts
@@ -4,11 +4,13 @@ export const TITLE_FONT_COLOR = '0D0D0D'
 export const TEXT_FONT_COLOR = '2A2A2A'
 
 export const FONT_FAMILY = `QuickSand`
+export const TITLE_FONT_WEIGHT = '600'
+export const TEXT_FONT_WEIGHT = '400'
 
 export const BOX_BACKGROUND_COLOR =
-  'linear-gradient(166.21deg, #DEDEDE -8.68%, #999999 100.91%, #999999 100.91%)'
+  'linear-gradient(166.21deg, rgba(222,222,222,0.02) 5%, rgba(153,153,153,0.13) 100%)'
 export const BOX_OPACITY = '0.1'
-export const BOX_SHADOW = 'rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;'
-export const BOX_BORDER = '2px solid #000000'
+export const BOX_SHADOW = 'rgba(99, 99, 99, 0.2) 1px 2px 5px 0px;'
+export const BOX_BORDER = '1px solid rgba(42, 42, 42, 0.04)'
 
 export const APP_FUNCTIONAL_WIDTH = '800px'
