1c3960d9fc034ba32fd91b2c3cc3d59006e8610e
ClementPremartin <clementpremartin@gmail.com>
2022-12-22 17:05:42 +0100

    setup style css variable

--- web-app/src/styles/style-constants.ts
+++ web-app/src/styles/style-constants.ts
@@ -1,4 +1,14 @@
-export const MAIN_THEME_COLOR = "#EFF0F3";
-export const BORDER_COLOR = "#C9C9C9";
+export const MAIN_THEME_COLOR = '#FF9442'
+export const BACKGROUND_COLOR = '#EFF0F3'
+export const TITLE_FONT_COLOR = '0D0D0D'
+export const TEXT_FONT_COLOR = '2A2A2A'
 
-export const APP_FUNCTIONAL_WIDTH = "800px";
+export const FONT_FAMILY = `QuickSand`
+
+export const BOX_BACKGROUND_COLOR =
+  'linear-gradient(166.21deg, #DEDEDE -8.68%, #999999 100.91%, #999999 100.91%)'
+export const BOX_OPACITY = '0.1'
+export const BOX_SHADOW = 'rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;'
+export const BOX_BORDER = '2px solid #000000'
+
+export const APP_FUNCTIONAL_WIDTH = '800px'
