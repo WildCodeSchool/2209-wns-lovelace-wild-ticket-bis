34f73c951c6939eeaf57b2b335d8742510b5b92c
ClementPremartin <clementpremartin@gmail.com>
2023-02-02 11:44:57 +0100

    create deleteSession method in sessionRepo

--- back-end/src/models/AppUser/Session.repository.ts
+++ back-end/src/models/AppUser/Session.repository.ts
@@ -8,7 +8,19 @@ export default class SessionRepository extends SessionDb {
     return this.saveSession(session);
   }
 
+  static async deleteSession(session: Session): Promise<Session> {
+    const existingSession = await this.findById(session.id);
+    if (!existingSession) {
+      throw new Error("No existing session for this User");
+    }
+    await this.repository.remove(existingSession);
+    // resetting ID because existingSession loses ID after calling remove
+    existingSession.id = session.id;
+    return existingSession;
+  }
+
   static findById(id: string): Promise<Session | null> {
     return this.repository.findOneBy({ id });
   }
 }
+
