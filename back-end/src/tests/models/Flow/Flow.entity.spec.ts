143b72f6c7abf174012070388dfe16a103f6c8d7
ClementPremartin <99645627+ClementPremartin@users.noreply.github.com>
2023-02-28 11:32:15 +0100

    Feature/setup GitHub actions
    Ajout du github actions pour back-end et web-app
    Désormais, lorsqu'on poussera le build et les tests se lanceront automatiquement.
    Nous ne pourront plus faire de merge si le build et les tests ne passent pas

---
+++ back-end/src/tests/models/Flow/Flow.entity.spec.ts
@@ -0,0 +1,10 @@
+import Flow from '../../../models/Flow/Flow.entity';
+import FlowRepository from '../../../models/Flow/Flow.repository';
+
+describe('Flow', () => {
+  describe('getFlowByName', () => {
+    describe('when user has flow', () => {
+      it('return flow by name', () => {});
+    });
+  });
+});
