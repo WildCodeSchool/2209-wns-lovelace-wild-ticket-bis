06cbbe597a84494c35a1928833faa8c61b1e0872
ClementPremartin <clementpremartin@gmail.com>
2023-03-01 15:05:10 +0100

    install react icons and setup style in ticket component

---
+++ web-app/src/pages/Tickets/Tickets.styled.tsx
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+import {
+  BUTTON_BOX_SHADOW,
+  FONT_FAMILY,
+  SECONDARY_BUTTON_COLOR,
+  TEXT_FONT_WEIGHT,
+} from 'styles/style-constants';
+
+export const ButtonAction = styled.button`
+  border: 0;
+  border-radius: 50px;
+  background-color: ${SECONDARY_BUTTON_COLOR};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-family: ${FONT_FAMILY};
+  font-weight: ${TEXT_FONT_WEIGHT};
+  box-shadow: ${BUTTON_BOX_SHADOW};
+  font-size: 1rem;
+  margin-right: 20px;
+  padding-top: 5px;
+  padding-bottom: 5px;
+  padding-left: 10px;
+  padding-right: 10px;
+`;
+
+export const ContainerButton = styled.div`
+  display: flex;
+  align-items: flex-end;
+  justify-content: space-between;
+  margin-bottom: 20px;
+`;
+
+export const ContainerButtonAction = styled.div`
+  display: flex;
+`;
--- web-app/src/pages/Tickets/Tickets.tsx
+++ web-app/src/pages/Tickets/Tickets.tsx
@@ -1,31 +1,98 @@
 import { useState } from 'react';
 import {
-  ContainerButton,
-  ButtonDelete,
-  LogoLinkButton,
-  LogoLinkButtonDisabled,
   MainContainer,
   ButtonAdd,
+  AllStatusContainer,
+  ArrayContainer,
+  ContainerInputItem,
+  Divider,
+  HeaderList,
+  InputItem,
+  ItemList,
+  ListContainer,
+  StatusContainer,
+  StatusNoScan,
+  TextElement,
+  TextElementHeader,
 } from 'pages/MesFlux/MesFlux.styled';
-import Corbeille from '../../assets/corbeille.png';
+import {
+  ButtonAction,
+  ContainerButton,
+  ContainerButtonAction,
+} from './Tickets.styled';
+import { GoTrashcan } from 'react-icons/go';
+import { IoIosPlay } from 'react-icons/io';
+import {
+  COLOR_ERROR_TICKET,
+  COLOR_VALIDATE_TICKET,
+  COLOR_WAITING_TICKET,
+} from 'styles/style-constants';
 
 const Tickets = () => {
-  const [allTicketsSelected, setAllTicketsSelected] = useState<Array<string>>(
-    []
-  );
+  const [allTicketsSelected] = useState<Array<string>>([]);
+  const [tickets] = useState([]);
   return (
     <MainContainer>
       <ContainerButton>
-        <ButtonDelete disabled={allTicketsSelected.length > 0 ? false : true}>
-          {allTicketsSelected.length > 0 ? (
-            <LogoLinkButton src={Corbeille}></LogoLinkButton>
-          ) : (
-            <LogoLinkButtonDisabled src={Corbeille}></LogoLinkButtonDisabled>
-          )}
-          Supprimer
-        </ButtonDelete>
+        <ContainerButtonAction>
+          <ButtonAction disabled={allTicketsSelected.length > 0 ? false : true}>
+            <GoTrashcan size={25} /> &ensp;Supprimer
+          </ButtonAction>
+          <ButtonAction disabled={allTicketsSelected.length > 0 ? false : true}>
+            {allTicketsSelected.length > 0 ? (
+              <IoIosPlay size={25} style={{ color: COLOR_WAITING_TICKET }} />
+            ) : (
+              <IoIosPlay size={25} />
+            )}
+            &ensp;En attente
+          </ButtonAction>
+          <ButtonAction disabled={allTicketsSelected.length > 0 ? false : true}>
+            {allTicketsSelected.length > 0 ? (
+              <IoIosPlay size={25} style={{ color: COLOR_VALIDATE_TICKET }} />
+            ) : (
+              <IoIosPlay size={25} />
+            )}
+            &ensp;Valider
+          </ButtonAction>
+          <ButtonAction disabled={allTicketsSelected.length > 0 ? false : true}>
+            {allTicketsSelected.length > 0 ? (
+              <IoIosPlay size={25} style={{ color: COLOR_ERROR_TICKET }} />
+            ) : (
+              <IoIosPlay size={25} />
+            )}
+            &ensp;Incident
+          </ButtonAction>
+        </ContainerButtonAction>
         <ButtonAdd>Ajouter un ticket</ButtonAdd>
       </ContainerButton>
+      <ArrayContainer>
+        <HeaderList>
+          <TextElementHeader></TextElementHeader>
+          <TextElementHeader>Heure</TextElementHeader>
+          <TextElementHeader>Numéro</TextElementHeader>
+          <TextElementHeader>Statut</TextElementHeader>
+          <TextElementHeader></TextElementHeader>
+        </HeaderList>
+        <Divider />
+        <ListContainer>
+          {tickets.map((ticket: any) => {
+            return (
+              <ItemList key={ticket.id}>
+                <ContainerInputItem>
+                  <InputItem></InputItem>
+                </ContainerInputItem>
+                <TextElement>15/10/22 11:35:56</TextElement>
+                <TextElement>{ticket.number}</TextElement>
+                <AllStatusContainer>
+                  <StatusContainer>
+                    <StatusNoScan></StatusNoScan>0
+                  </StatusContainer>
+                </AllStatusContainer>
+              </ItemList>
+            );
+          })}
+        </ListContainer>
+      </ArrayContainer>
     </MainContainer>
   );
 };
