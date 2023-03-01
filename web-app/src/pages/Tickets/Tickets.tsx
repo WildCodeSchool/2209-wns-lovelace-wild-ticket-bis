f96a84246e8f853a934f068b5edb45fbbe3b4ba6
ClementPremartin <clementpremartin@gmail.com>
2023-03-01 10:24:17 +0100

    add fisrt ticket button in front

--- web-app/src/pages/Tickets/Tickets.tsx
+++ web-app/src/pages/Tickets/Tickets.tsx
@@ -1,5 +1,33 @@
+import { useState } from 'react';
+import {
+  ContainerButton,
+  ButtonDelete,
+  LogoLinkButton,
+  LogoLinkButtonDisabled,
+  MainContainer,
+  ButtonAdd,
+} from 'pages/MesFlux/MesFlux.styled';
+import Corbeille from '../../assets/corbeille.png';
+
 const Tickets = () => {
-  return <div>Tickets</div>;
+  const [allTicketsSelected, setAllTicketsSelected] = useState<Array<string>>(
+    []
+  );
+  return (
+    <MainContainer>
+      <ContainerButton>
+        <ButtonDelete disabled={allTicketsSelected.length > 0 ? false : true}>
+          {allTicketsSelected.length > 0 ? (
+            <LogoLinkButton src={Corbeille}></LogoLinkButton>
+          ) : (
+            <LogoLinkButtonDisabled src={Corbeille}></LogoLinkButtonDisabled>
+          )}
+          Supprimer
+        </ButtonDelete>
+        <ButtonAdd>Ajouter un ticket</ButtonAdd>
+      </ContainerButton>
+    </MainContainer>
+  );
 };
 
 export default Tickets;
