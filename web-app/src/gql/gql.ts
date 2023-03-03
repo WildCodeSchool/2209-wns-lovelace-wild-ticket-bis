1079572d1b1879f0fce495659a57dc0cc35d6fd7
ClementPremartin <clementpremartin@gmail.com>
2023-03-03 17:30:01 +0100

    connect add tickets flow in front

--- web-app/src/gql/gql.ts
+++ web-app/src/gql/gql.ts
@@ -20,6 +20,7 @@ const documents = {
     "\n  mutation SignIn($emailAddress: String!, $password: String!) {\n    signIn(emailAddress: $emailAddress, password: $password) {\n      id\n      emailAddress\n      firstName\n      lastName\n    }\n  }\n": types.SignInDocument,
     "\n  mutation SignUp(\n    $firstName: String!\n    $lastName: String!\n    $emailAddress: String!\n    $password: String!\n  ) {\n    signUp(\n      firstName: $firstName\n      lastName: $lastName\n      emailAddress: $emailAddress\n      password: $password\n    ) {\n      id\n      emailAddress\n    }\n  }\n": types.SignUpDocument,
     "\n  query GetTicketsByFlowId($flowId: String!) {\n    getTicketsByFlowId(flowId: $flowId) {\n      flowName\n      id\n      tickets {\n        date\n        id\n        isTrash\n        status\n      }\n    }\n  }\n": types.GetTicketsByFlowIdDocument,
+    "\n  mutation AddTicketByFlowId($flowId: String!) {\n    addTicketByFlowId(flowId: $flowId) {\n      date\n      id\n      isTrash\n      status\n    }\n  }\n": types.AddTicketByFlowIdDocument,
 };
 
 /**
@@ -64,6 +65,10 @@ export function graphql(source: "\n  mutation SignUp(\n    $firstName: String!\n
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
 export function graphql(source: "\n  query GetTicketsByFlowId($flowId: String!) {\n    getTicketsByFlowId(flowId: $flowId) {\n      flowName\n      id\n      tickets {\n        date\n        id\n        isTrash\n        status\n      }\n    }\n  }\n"): (typeof documents)["\n  query GetTicketsByFlowId($flowId: String!) {\n    getTicketsByFlowId(flowId: $flowId) {\n      flowName\n      id\n      tickets {\n        date\n        id\n        isTrash\n        status\n      }\n    }\n  }\n"];
+/**
+ * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ */
+export function graphql(source: "\n  mutation AddTicketByFlowId($flowId: String!) {\n    addTicketByFlowId(flowId: $flowId) {\n      date\n      id\n      isTrash\n      status\n    }\n  }\n"): (typeof documents)["\n  mutation AddTicketByFlowId($flowId: String!) {\n    addTicketByFlowId(flowId: $flowId) {\n      date\n      id\n      isTrash\n      status\n    }\n  }\n"];
 
 export function graphql(source: string) {
   return (documents as any)[source] ?? {};
