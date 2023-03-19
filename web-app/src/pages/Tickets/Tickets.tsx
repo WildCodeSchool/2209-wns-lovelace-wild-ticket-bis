1425de691d1044ec12d6c2d7cd465e66e1a27f35
ClementPremartin <clementpremartin@gmail.com>
2023-03-19 19:04:26 +0100

    fix date displaying

--- web-app/src/pages/Tickets/Tickets.tsx
+++ web-app/src/pages/Tickets/Tickets.tsx
@@ -136,7 +136,15 @@ const Tickets = () => {
 
   const convertDateFormat = (isoDate: string) => {
     const date = new Date(isoDate);
-    return `${date.getDay()}/${date.getMonth()}/${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    const dateResult = date.toLocaleDateString('en-Gb', {
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+    });
+    return dateResult;
   };
 
   const convertIdFormat = (id: string) => {
