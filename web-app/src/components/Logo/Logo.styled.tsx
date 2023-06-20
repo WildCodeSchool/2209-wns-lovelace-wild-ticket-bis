40dc2d0ff06731c6e35a51125b4ba6743c831988
ClementPremartin <clementpremartin@gmail.com>
2023-06-20 12:14:35 +0200

    [Fix] Logo displaying on desktop

--- web-app/src/components/Logo/Logo.styled.tsx
+++ web-app/src/components/Logo/Logo.styled.tsx
@@ -4,16 +4,14 @@ import { MAX_WIDTH_PHONE, MAX_WIDTH_TABLET } from 'styles/style-constants';
 export const GlobalLogoContainer = styled.div`
   grid-area: 1 / 1 / 2 / 2;
   display: grid;
-  @media (max-width: ${MAX_WIDTH_TABLET}) {
-    display: ${(props) => (props.hidden ? 'none' : 'grid')};
-  }
 `;
 
 export const LogoImg = styled.img`
   width: 120px;
   margin: 20px;
+  display: flex;
   @media (max-width: ${MAX_WIDTH_TABLET}) {
-    width: ${(props) => (props.hidden ? '120px' : '100px')};
+    display: none;
   }
 `;
 
