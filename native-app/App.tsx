1224986f89a9d36729d27583e0549a12c854c97f
ClementPremartin <clementpremartin@gmail.com>
2023-06-21 13:32:08 +0200

    [Core] config gql

--- native-app/App.tsx
+++ native-app/App.tsx
@@ -2,13 +2,53 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { BACKGROUND_COLOR } from './styles/style-constants';
+import {
+  ApolloClient,
+  ApolloProvider,
+  HttpLink,
+  InMemoryCache,
+  split,
+} from '@apollo/client';
+import { getMainDefinition } from '@apollo/client/utilities';
+import { createClient } from 'graphql-ws';
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
+import { WS_DEV } from './config';
+
+const httpLink = new HttpLink({
+  uri: '/api',
+});
+
+const wsLink = new GraphQLWsLink(
+  createClient({
+    url: WS_DEV,
+  })
+);
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink
+);
+
+const client = new ApolloClient({
+  link: splitLink,
+  cache: new InMemoryCache(),
+});
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Welcome in Flux !</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ApolloProvider client={client}>
+      <View style={styles.container}>
+        <Text>Welcome to Flux !</Text>
+        <StatusBar style="auto" />
+      </View>
+    </ApolloProvider>
   );
 }
 
