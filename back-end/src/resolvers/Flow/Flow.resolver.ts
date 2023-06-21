aba74a56af7cde58036df9fb74cda7aba828b790
ClementPremartin <clementpremartin@gmail.com>
2023-06-21 10:38:30 +0200

    [Feat] verify api input parameters with class validator

--- back-end/src/resolvers/Flow/Flow.resolver.ts
+++ back-end/src/resolvers/Flow/Flow.resolver.ts
@@ -1,9 +1,18 @@
-import { Arg, Args, Int, Mutation, Query, Resolver } from 'type-graphql';
+import {
+  Arg,
+  Args,
+  Authorized,
+  Int,
+  Mutation,
+  Query,
+  Resolver,
+} from 'type-graphql';
 import Flow from '../../models/Flow/Flow.entity';
-import { AddFlowArgs, getTicketsByFlowIdArgs } from './Flow.input';
 import FlowRepository from '../../models/Flow/Flow.repository';
+import { AddFlowArgs, getTicketsByFlowIdArgs } from './Flow.input';
 @Resolver(() => Flow)
 export default class FlowResolver {
+  @Authorized()
   @Query(() => Flow)
   getTicketsByFlowId(
     @Args() { flowId }: getTicketsByFlowIdArgs
@@ -11,11 +20,13 @@ export default class FlowResolver {
     return FlowRepository.getFlowById(flowId);
   }
 
+  @Authorized()
   @Mutation(() => Flow)
   addFlow(@Args() { id, flowName }: AddFlowArgs): Promise<Flow> {
     return FlowRepository.createFlow(flowName, id);
   }
 
+  @Authorized()
   @Mutation(() => Int)
   deleteFlow(
     @Arg('arrayId', () => [String]) arrayId: string[]
