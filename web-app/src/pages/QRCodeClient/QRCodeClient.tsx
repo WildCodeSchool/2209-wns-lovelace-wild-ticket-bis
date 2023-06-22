ee32d0d580f039f22d7a84522dacc641831e451b
MidNightRoad <bergerpierre09@gmail.com>
2023-06-22 11:24:30 +0200

    rebase

--- web-app/src/pages/QRCodeClient/QRCodeClient.tsx
+++ web-app/src/pages/QRCodeClient/QRCodeClient.tsx
@@ -2,12 +2,24 @@ import { useEffect } from 'react';
 import {
   ContainerLogoLarge,
   ContainerQrCodeClient,
+  ContainerText,
+  ContainerTicketNumber,
   LeftSideQrCodeClient,
   LogoLarge,
+  NumberTicket,
+  QRCodeClientElementContainer,
+  QrCodeContainer,
+  QrCodeShadow,
   RightSideQrCodeClient,
+  TextLinkQrCode,
+  TextQrCodeClient,
+  TextScanQrCode,
+  TextTicketNumber,
+  TextTitleLinkQrCode,
 } from './QrCodeClient.styled';
 import logoLarge from '../../assets/logo_flux_large.png';
 import { PropsDisplayNavbar } from 'utils';
+import { QRCodeSVG } from 'qrcode.react';
 
 const QRCodeClient = ({ displayNavbar }: PropsDisplayNavbar) => {
   useEffect(() => {
@@ -23,8 +35,39 @@ const QRCodeClient = ({ displayNavbar }: PropsDisplayNavbar) => {
         <ContainerLogoLarge>
           <LogoLarge src={logoLarge}></LogoLarge>
         </ContainerLogoLarge>
+        <ContainerText>
+          <TextQrCodeClient>
+            Soyez prévenue quand votre commande est prête ! 👋
+          </TextQrCodeClient>
+        </ContainerText>
+        <ContainerTicketNumber>
+          <TextTicketNumber>
+            Numero d’attente : <br />
+            <NumberTicket>#5842</NumberTicket>
+          </TextTicketNumber>
+        </ContainerTicketNumber>
       </LeftSideQrCodeClient>
-      <RightSideQrCodeClient></RightSideQrCodeClient>
+      <RightSideQrCodeClient>
+        <QrCodeContainer>
+          <TextScanQrCode>
+            • &nbsp; Scanner le Qr-code en dessous 👇
+          </TextScanQrCode>
+          <QRCodeClientElementContainer>
+            <QrCodeShadow>
+              <QRCodeSVG
+                value={`https://localhost:3000/qr-code-client/
+              )}`}
+                bgColor={'transparent'}
+                size={400}
+              />
+            </QrCodeShadow>
+          </QRCodeClientElementContainer>
+          <TextTitleLinkQrCode>
+            • &nbsp; Ou rendez-vous sur : <br />
+            <TextLinkQrCode>https://NameOfApp/ticket/idticket</TextLinkQrCode>
+          </TextTitleLinkQrCode>
+        </QrCodeContainer>
+      </RightSideQrCodeClient>
     </ContainerQrCodeClient>
   );
 };
--- web-app/src/pages/QRCodeClient/QrCodeClient.styled.tsx
+++ web-app/src/pages/QRCodeClient/QrCodeClient.styled.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { FONT_FAMILY } from 'styles/style-constants';
 
 export const ContainerQrCodeClient = styled.div`
   height: 100vh;
@@ -7,18 +8,116 @@ export const ContainerQrCodeClient = styled.div`
 
 export const RightSideQrCodeClient = styled.div`
   height: 100%;
-  width: 50%;
-  background: aquamarine;
+  width: 60%;
 `;
 export const LeftSideQrCodeClient = styled.div`
   height: 100%;
-  width: 50%;
+  width: 40%;
 `;
 
 export const ContainerLogoLarge = styled.div`
-  border: 1px solid blue;
+  height: 30%;
 `;
 
 export const LogoLarge = styled.img`
-  border: 1px solid red;
+  height: auto;
+  width: 40%;
+  margin: 8% 0 0 8%;
+`;
+
+export const ContainerText = styled.div`
+  
+  height: 40%;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+}
+`;
+export const TextQrCodeClient = styled.h2`
+  width: 70%;
+  font-family: ${FONT_FAMILY};
+  font-weight: bolder;
+  margin: 0 0 0 8%;
+  font-size: 3.5rem;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+export const ContainerTicketNumber = styled.div`
+  height: 30%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const TextTicketNumber = styled.h2`
+  font-family: ${FONT_FAMILY};
+  font-weight: initial;
+  text-align: center;
+  font-size: 3.5rem;
+`;
+export const NumberTicket = styled.h2`
+  font-family: ${FONT_FAMILY};
+  font-weight: bold;
+  margin: 0;
+  font-size: 3.5rem;
+`;
+export const QrCodeContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+`;
+
+export const TextTicketNumberQrCode = styled.h2`
+  font-family: ${FONT_FAMILY};
+  font-weight: bolder;
+  font-size: 2.5em;
+  margin: 8% 0 0 0;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+export const TextScanQrCode = styled.h2`
+  font-family: ${FONT_FAMILY};
+  font-weight: bolder;
+  font-size: 2.5em;
+  margin: 8% 0 0 0;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+export const TextTitleLinkQrCode = styled.h2`
+  font-family: ${FONT_FAMILY};
+  font-weight: bolder;
+  font-size: 2.5em;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+export const QrCodeShadow = styled.div`
+  padding: 5%;
+  border-radius: 30px;
+  background: linear-gradient(
+    127deg,
+    rgba(255, 140, 56, 0.16850490196078427) 47%,
+    rgba(255, 138, 53, 0) 100%
+  );
+`;
+
+export const TextLinkQrCode = styled.h2`
+  font-family: ${FONT_FAMILY};
+  font-weight: bolder;
+  font-size: 1em;
+  text-align: center;
+`;
+
+export const QRCodeClientElementContainer = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
 `;
