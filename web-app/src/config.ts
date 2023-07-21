f2dfb64e82c7f09d936c800f2edd4fbc35f59ec9
ClementPremartin <clementpremartin@gmail.com>
2023-07-21 14:17:59 +0200

    [Fix] Url dev

--- web-app/src/config.ts
+++ web-app/src/config.ts
@@ -10,6 +10,6 @@ export const WS_DEV = 'ws://localhost:4000/ws';
 
 export const URL_PROD = 'https://staging.lovelace5.wns.wilders.dev/';
 
-export const URL_DEV = 'https://localhost:3000/';
+export const URL_DEV = 'http://localhost:3000/';
 
 export { NODE_ENV, IS_PRODUCTION, IS_DEVELOPEMENT };
